36608ef02db5b91bb43117becdf25afb46535683
OthmanAmoudi <red.fantastic@hotmail.com>
2023-10-02 22:11:10 +0300

    add .env

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,13 @@
 import swagger from '@elysiajs/swagger';
 import { TasksController } from './modules';
-import { requestLogger, registerControllers } from './utils';
+import { requestLogger } from './utils';
 
 export function bootServer(app: any) {
   // middlewates
   app.onResponse(requestLogger);
   app.use(swagger());
   //controllers
-  registerControllers(app, [TasksController]); 
-  // or 
-  // app.use(TasksController.start())
+  app.use(TasksController.start());
   // app.use(AnotherController.start())
-  
-// TODO: handle custom middlewares in controllers
 }
+// TODO: handle custom middlewares in controllers
